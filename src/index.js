8ee0cf1a82a11deb06d194884294b313395e630a
Niks0n <nikitab@youhunted.com>
2021-06-08 15:24:32 +0300

    v1.0

--- src/index.js
+++ src/index.js
@@ -3,18 +3,10 @@ createPlayer();
 function createPlayer() {
   const dp = new DPlayer({
     container: document.getElementById("dplayer"),
-    screenshot: true,
     video: {
       url: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
-      pic: "../pic.jpg",
-      thumbnails: "thumbnails.jpg",
     },
-    // subtitle: {
-    //   url: "webvtt.vtt",
-    // },
-    // danmaku: {
-    //   id: "demo",
-    //   api: "https://api.prprpr.me/dplayer/",
-    // },
   });
+
+  dp.play();
 }
